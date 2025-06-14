f9bca920286a0d6e7b5ab6fd9caf09135463a492
amoremea <tstoneue@gmail.com>
2025-06-14 06:05:47 +0500

    Socket url updated

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
 	useEffect(() => {
 		if (authUser) {
-			const socket = io("localhost:5000", {
+			const socket = io("https://poopingmessage.onrender.com/", {
 				query: {
 					userId: authUser._id,
 				},
